0fa4e61e6e1c3e5906c58afbf2f312b350ffb793
Stephane_Branly <stephanebranly.pro@gmail.com>
2021-06-01 17:30:17 +0200

    fix: test template

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@
 #                                                       +++##+++::::::::::::::       +#+    +:+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       +#+    +#+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       #+#    #+#     #+#     #+#    #+#      #
-#      Update: 2021/06/01 16:16:24 by branlyst & duranma  ::::::::::::::::::::        ########      ###      ######## .fr   #
+#      Update: 2021/06/01 17:29:43 by branlyst & duranma  ::::::::::::::::::::        ########      ###      ######## .fr   #
 #                                                                                                                           #
 # ************************************************************************************************************************* #
 
@@ -36,13 +36,19 @@ def extraire_fichiers_map_test() -> List[str]:
     return liste_fichiers
 
 def test():
-    print(f"{couleurs.ENTETE}Running tests{couleurs.FIN}")
-    for fichier in extraire_fichiers_map_test():
+    print(f"{couleurs.ENTETE}Tests en cours{couleurs.FIN}")
+    compteur_ok: int = 0
+    liste_fichiers: List[str] = extraire_fichiers_map_test()
+    for fichier in liste_fichiers:
         temps_depart: float = time.time()
         print(f"{couleurs.ATTENTION}Test map {fichier}\t...{couleurs.FIN}", end='\r')
         time.sleep(1)
         temps_ecoule: str = format(time.time() - temps_depart, '.3f') + "s"
         if output():
+            compteur_ok += 1
             print(f"{couleurs.FIN}Test map {fichier}\t{couleurs.OKVERT}OK {couleurs.FIN}\t{couleurs.GRAS}[{temps_ecoule}]{couleurs.FIN}")
         else:
             print(f"{couleurs.FIN}Test map {fichier}\t{couleurs.KO}KO {couleurs.FIN}\t{couleurs.GRAS}[{temps_ecoule}]{couleurs.FIN}")
+        
+    print(f"{couleurs.ENTETE}Tests finis{couleurs.FIN}\t\t{couleurs.GRAS}{compteur_ok}/{len(liste_fichiers)} OK{couleurs.FIN}")
+    
